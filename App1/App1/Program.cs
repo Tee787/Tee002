ea87aa3ff4b1565981b8a066acad12eb7ff0eb3a
Tawhirimatea Tamihana <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-07-25 19:17:48 +1200

    second commit

---
+++ App1/App1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Intro start to c# programming. Writing your first code
+            Console.WriteLine("Hello World"); // Here is where you start to write your first code 
+            Console.ReadLine(); // You need this to view your first code
+        }
+    }
+}
