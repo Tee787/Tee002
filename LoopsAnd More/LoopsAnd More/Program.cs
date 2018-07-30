ed2289e3cef5ec0aac44680c947bb6ea93c2d178
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-07-30 20:44:36 +1200

    Third commit

---
+++ LoopsAnd More/LoopsAnd More/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsAnd_More
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine("Enter number: ");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Value of number = " + number);
+
+                number++;
+
+            }
+            while (number <= 20);
+
+            Console.ReadLine();
+        }
+    }
+}
