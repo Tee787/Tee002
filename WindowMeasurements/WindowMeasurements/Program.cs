1f2fad874153e90db29073a75cf991ef8ac1f2d5
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-07-30 20:35:26 +1200

    second commit

---
+++ WindowMeasurements/WindowMeasurements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowMeasurements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width, height, woodLength, glassArea;
+
+            string widthString, heightString;
+
+            Console.WriteLine("Enter width of window then press Enter");
+
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.WriteLine("Enter height of window then press Enter");
+
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height);
+
+            glassArea = width * height;
+
+
+            Console.WriteLine("The length of the wood is " + woodLength + " metres.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("The area of the glass is " + glassArea + "metres squared");
+
+        }
+    }
+}
